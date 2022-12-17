f3cc6f20bc8b9ebf8e13060626d4b60f7d44c910
Jonathon Powell <jonathonwpowell@gmail.com>
2022-12-16 22:25:15 -0500

    Update site with 22-23 sheets

--- src/common/api-config.js
+++ src/common/api-config.js
@@ -4,6 +4,8 @@ export default {
   //spreadsheetId: "12K-dZ5_FnyVbcPrqWmjDDBodOyKIJ1Zi_EOaXCTz2G0"
   //spreadsheetId: "1nWVIVz4Nj3oJSq2Ch0WmX_WbRf7j2qbWb8CkghdiTEM" //18-19
   // spreadsheetId: "168G9srsw3-Y1q2Yv5jvw6CWlrDLK1vRW7TgEEPAfSL0", //19-20
-  spreadsheetId: "1CxEaDd-Ds1JkNA-uXK5GPqt7c_rjZOd27Y2CnOysej4", // 21-22
-  gameScheduleSheetId: "1I2UD_pn8ivV_rGB7LC31VJIHL2WMiU9E8gFjCKjc3kw", //21-22
+  // spreadsheetId: "1CxEaDd-Ds1JkNA-uXK5GPqt7c_rjZOd27Y2CnOysej4", // 21-22
+  spreadsheetId: "1L32WTQwyfgILGq25WXU23_G3iqFmFbIWpuTJMii-2V8", // 22-23
+  // gameScheduleSheetId: "1I2UD_pn8ivV_rGB7LC31VJIHL2WMiU9E8gFjCKjc3kw", //21-22
+  gameScheduleSheetId: "11eb92qvrFCsEPT_pK1LujAGj2GZh7bMEMKWT_Sew31A", // 22-23
 };
