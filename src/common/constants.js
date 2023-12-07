dee4980c39f8b9c8793fcd70d38b44d08f10c06c
Jonathon Powell <jonathonwpowell@gmail.com>
2023-12-07 17:58:57 -0500

    Update constants for 2023 2024

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,10 +1,10 @@
 export default {
-    pickUrl: "https://forms.gle/phV4DHJ6gkP8Ds9m6",
+    pickUrl: "https://forms.gle/wA53b1LQiDd4U2pk8",
     paypalUrl: "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=bowlpooldirector%40gmail%2ecom&lc=US&item_name=Bowl%20Pool%20Central&amount=5%2e50&currency_code=USD&button_subtype=services&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted",
     venmoUser: "@Cameron-Powell-38",
-    rulesUrl: "https://docs.google.com/document/d/e/2PACX-1vSQEn8p-zU2a-CiF63aP_7ULSS0PNeXzBOXnTUeQGhGyKrg3oq3pDJJ0Uk3w4TJNVjR3Y1rYOCioQlJ/pub?embedded=true",
-    firstGameTime: "Dec. 16th at 11:30 AM",
+    rulesUrl: "https://docs.google.com/document/d/e/2PACX-1vTIzeVmIwu7my0b7FQ65quyVVlge_-uTAsmNeroi_zP_oF_TShzZxugMHdOp7fZ-9VQEChHnkJHhrrD/pub?embedded=true",
+    firstGameTime: "Dec. 16th at 11:00 AM Eastern",
     multiplayerUrlParam: "displayPlayers",
-    // yearIfOld: " (2021 - 2022)",
-    yearIfOld: "",
+    yearIfOld: " (2022 - 2023)",
+    // yearIfOld: "",
 }
