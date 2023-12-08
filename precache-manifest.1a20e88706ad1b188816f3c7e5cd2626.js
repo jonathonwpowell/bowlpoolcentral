2e7189b0e74d62129ad2f542566b8c7577d6163e
Jonathon Powell <jonathonwpowell@gmail.com>
2023-12-07 21:02:07 -0500

    Updates

---
+++ precache-manifest.1a20e88706ad1b188816f3c7e5cd2626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98cc7162638154fa994",
+    "url": "/static/css/main.2e4853e2.chunk.css"
+  },
+  {
+    "revision": "a98cc7162638154fa994",
+    "url": "/static/js/main.a98cc716.chunk.js"
+  },
+  {
+    "revision": "e54f780124d258f11646",
+    "url": "/static/js/1.e54f7801.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4bab744aece397173d96ad98d9021481",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b6afd35b18a15ae94a0f24cb9344d4d.js"
+  "/precache-manifest.1a20e88706ad1b188816f3c7e5cd2626.js"
 );
 
 workbox.clientsClaim();
